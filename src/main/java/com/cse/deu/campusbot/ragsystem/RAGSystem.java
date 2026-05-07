546a011f423f79443fdf35e0ff3afeba5e07c4f1
WONSIK-GIT <wonsik4372@naver.com>
2026-05-08 08:49:19 +0900

    first commit

---
+++ src/main/java/com/cse/deu/campusbot/ragsystem/RAGSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cse.deu.campusbot.ragsystem;
+
+/**
+ *
+ * @author wonsik
+ */
+public class RAGSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
